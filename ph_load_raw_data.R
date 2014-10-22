1d0c1ad914cfc7a7edd3547ba1d919ecca1dd4fe
aliaksey <aliaksey.vasilevich@gmail.com>
2014-10-22 11:35:15 +0200

    addinf feature column to cell shape data

--- ph_load_raw_data.R
+++ ph_load_raw_data.R
@@ -1,3 +1,7 @@
+##reading csv tables
 cell.ftrs<-read.csv("cell_nuclei_features.csv")
 image.data<-read.csv("image_data.csv")
+## adding feature idx to cellshape data
+colnames(image.data)[which(colnames(image.data)=='Image_Metadata_featureidx')]<- 'FeatureIdx'
+cell.ftrs<-merge(cell.ftrs,image.data[,c("ImageNumber", "FeatureIdx")], by="ImageNumber")
 save(cell.ftrs,image.data, file="ph_raw_data.RData")
