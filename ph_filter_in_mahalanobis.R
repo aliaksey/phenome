445c89b4eb56af225533f40ee7f931386623032e
aliaksey <aliaksey.vasilevich@gmail.com>
2014-10-22 16:36:23 +0200

    script to perform outliers detection in mahalanobis distance with cell shape noncorrelated features

---
+++ ph_filter_in_mahalanobis.R
@@ -0,0 +1,48 @@
+library(chemometrics)
+library(caret)
+
+load("ph_raw_data.RData")
+load("Cell_area_perim_corr.RData")
+##selecting cell shape variables
+all.names.temp<-names(cell.ftrs)
+#selecting only names contains cells_shape
+all.names.cell.t<-all.names.temp[grep("Cells_AreaShape", all.names.temp) ]
+# removing numbers and location features
+shape.col.names<-all.names.cell.t[!grepl("Center", all.names.cell.t)&
+                                   !grepl("Neighbors", all.names.cell.t)]
+cell.shape.temp<-cell.ftrs[,c("ImageNumber",  "FeatureIdx",shape.col.names)]
+#applying previous filters
+cell.shape<-cell.shape.temp[cell.shape.temp$ImageNumber%in%cell.area.f$ImageNumber&
+                              row.names(cell.shape.temp) %in% row.names(cell.area.f),]
+rm(list=c("cell.ftrs","image.data","cell.area.f"))
+##changing orientation parameter
+cell.shape$Cells_AreaShape_Orientation<-abs(cell.shape$Cells_AreaShape_Orientation)
+##find and remove highly correlated features
+#scale all the features
+cell.shape.data<-cell.shape[,!(colnames(cell.shape) %in% c("ImageNumber", "FeatureIdx"))]
+cntr<-apply(cell.shape.data,2,function(x) median(x))
+scl<-apply(cell.shape.data,2,function(x) mad(x))
+datMy.scale<- scale(cell.shape.data,
+                    center=cntr,scale=scl);
+#constructing correlation matrix
+corMatMy <- cor(datMy.scale)
+library(corrplot)
+#corrplot(corMatMy, order = "hclust", tl.cex=0.1)
+#visualize the matrix, clustering features by correlation index.
+highlyCor <- findCorrelation(corMatMy, 0.7)
+#Apply correlation filter at 0.70,
+#then we remove all the variable correlated with more 0.7.
+datMyFiltered.scale <- datMy.scale[,-highlyCor]
+corMatMy <- cor(datMyFiltered.scale)
+#corrplot(corMatMy, order = "hclust",tl.cex=0.5)
+#finding variables that are not correlated
+notcorrelatfeatures<-names(cell.shape.data[,-highlyCor])
+cell.shape.f<-c()
+for(i in unique(cell.shape[,"FeatureIdx"])){
+  temp2<-cell.shape[cell.shape$FeatureIdx==i,]
+  mdres<-Moutlier(temp2[,notcorrelatfeatures], quantile = 0.99, plot = F)
+  rsltmd<-temp2[mdres$rd < mdres$cutoff,]
+  mordisf<-rbind(cell.shape.f,rsltmd)
+  }
+save(cell.shape.f,file="Cell_shape_corr.RData")
+
