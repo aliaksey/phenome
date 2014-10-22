50f0fcaaa53cf82fbb7d71e1baafcce33be58d80
aliaksey <aliaksey.vasilevich@gmail.com>
2014-10-22 12:06:26 +0200

    cell density correction script

---
+++ ph_filter_cell_density.R
@@ -0,0 +1,17 @@
+##loading data
+load("ph_raw_data.RData")
+#filtering images based on cell number
+cell.density<-image.data[,c("ImageNumber", "Image_Count_Cells", "FeatureIdx" )]
+rm(list=c("cell.ftrs","image.data"))
+cell.dns.f<-c()
+for(i in unique(cell.density[,"FeatureIdx"])){
+  temp2<-cell.density[cell.density$FeatureIdx==i,]
+  tempcn<-unique(temp2[,c("ImageNumber","Image_Count_Cells")])
+  lbnd<-as.numeric(quantile(tempcn[,"Image_Count_Cells"], probs = 0.25))
+  ubnd<-as.numeric(quantile(tempcn[,"Image_Count_Cells"], probs = 0.75))
+  iud<-ubnd-lbnd
+  rsltc<-temp2[temp2[,"Image_Count_Cells"]<(ubnd+1.5*iud)&
+                 temp2[,"Image_Count_Cells"]>(lbnd-1.5*iud),]
+  cell.dns.f<-rbind(cell.dns.f,rsltc) 
+}
+save(cell.dns.f,file="Cell_dens_corr.RData")
