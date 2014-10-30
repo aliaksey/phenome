e91a469444a72d7b70b2a21e4eb72ebd8b2e9e3a
aliaksey <aliaksey.vasilevich@gmail.com>
2014-10-30 14:55:19 +0100

    added" 1. averaging evrything to image number 2. merging to data sets

--- ph_scale_data.R
+++ ph_scale_data.R
@@ -1,8 +1,24 @@
 rm(list=ls())
 load("ph_raw_data.RData")
+load("Cell_image reprod.RData")
 load("Cell_shape_corr.RData")
 ##changing orientation parameter
-cell.shape$Cells_AreaShape_Orientation<-abs(cell.shape$Cells_AreaShape_Orientation)
+cell.ftrs$Cells_AreaShape_Orientation<-abs(cell.ftrs.$Cells_AreaShape_Orientation)
+
+#applying previous filters
+image.data.f<-image.data[image.data$ImageNumber%in%cell.ftrs.reprod$ImageNumber,]
+
+cell.ftrs.f<-cell.ftrs[cell.ftrs$ImageNumber%in%cell.ftrs.reprod$ImageNumber&
+                              row.names(cell.ftrs) %in% row.names(cell.shape.f),]
+#make small comparison
+#plot(density(cell.ftrs[cell.ftrs$FeatureIdx==975,"Cells_AreaShape_Eccentricity"]))
+#plot(density(cell.ftrs.f[cell.ftrs.f$FeatureIdx==975,"Cells_AreaShape_Eccentricity"]))
+rm(list=c("cell.ftrs","image.data","cell.ftrs.reprod","cell.shape.f"))
+##aggregating data to image number
+image.ftrs.f<-aggregate(.~ImageNumber, data=cell.ftrs.f, median)
+##merging 2data sets
+
+image.allftrs<-merge(image.data.f, image.ftrs.f, by="ImageNumber")
 
 ##selecting cell shape variables
 all.names.temp<-names(cell.ftrs)
@@ -13,10 +29,6 @@ shape.col.names<-all.names.cell.t[!grepl("Center", all.names.cell.t)&
                                     !grepl("Neighbors", all.names.cell.t)&
                                     !grepl("Zernike", all.names.cell.t)]
 cell.shape.temp<-cell.ftrs[,c("ImageNumber","ObjectNumber", "FeatureIdx",shape.col.names)]
-#applying previous filters
-cell.shape<-cell.shape.temp[cell.shape.temp$ImageNumber%in%cell.area.f$ImageNumber&
-                              row.names(cell.shape.temp) %in% row.names(cell.area.f),]
-rm(list=c("cell.ftrs","image.data","cell.area.f"))
 ##find and remove highly correlated features
 #scale all the features
 cell.shape.data<-cell.shape[,!(colnames(cell.shape) %in% c("ImageNumber", "ObjectNumber",
