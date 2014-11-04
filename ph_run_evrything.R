59f1c6ba9d4190496bc57b171e062857bf72b1ad
aliaksey <aliaksey.vasilevich@gmail.com>
2014-11-04 16:16:19 +0100

    creating control data set added

--- ph_run_evrything.R
+++ ph_run_evrything.R
@@ -4,4 +4,5 @@ source("ph_filter_extreme_missegmentataion.R")
 source("ph_filter_in_mahalanobis.R")
 source("ph_find_reproducible_repeats.R")
 source("ph_scale_data.R")
+source("ph_creating ground trugh data set.R")
  
