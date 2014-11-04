2c823411216bfd71374a99d148e86722b89db63b
aliaksey <aliaksey.vasilevich@gmail.com>
2014-11-04 14:37:26 +0100

    added step to select top 1% of data

--- ph_creating ground trugh data set.R
+++ ph_creating ground trugh data set.R
@@ -30,4 +30,36 @@ for (k in 1:length(varlist)) {
   } 
  }
 save(escoreU, freqU, file="frequency and enrichment score for classes.RData")
- 
+rm(list=ls()) 
+##selecting top  for ground truth
+load("frequency and enrichment score for classes.RData")
+##select top 2% of data that have highest enrichment score
+esidx<-0.98
+
+grnd_trth<-as.data.frame(rbind(cbind(escoreU[escoreU[,"Branched"]>quantile(escoreU$Branched, prob=esidx), "ImageNumber"] ,"branched"), 
+                 cbind(escoreU[escoreU[,"Multipolar"]>quantile(escoreU$Multipolar, prob=esidx) ,"ImageNumber"],"multipolar"),
+                 cbind(escoreU[escoreU[,"Pancake"]>quantile(escoreU$Pancake, prob=esidx) ,"ImageNumber"],"pancake"),
+                 cbind(escoreU[escoreU[,"Spread_pancake"]>quantile(escoreU$Spread_pancake, prob=esidx), "ImageNumber"], "spancake"),
+                 cbind(escoreU[escoreU[,"Sticks"]>quantile(escoreU$Sticks, prob=esidx), "ImageNumber"], "sticks")))
+colnames(grnd_trth)<-c("IamgeNumber", "Class")
+#summary(grnd_trth)
+
+save(grnd_trth, file="Images for Control ground  Truth.Rdata")
+
+
+##old approach
+# grnd_trth=rbind(branched=matrix(c(na.omit(escoreU[escoreU[,2]>1 & freqU[,2]==1,1])
+#                               ,rep("branched",length
+#                                    (na.omit(escoreU[escoreU[,2]>1 & freqU[,2]==1,1])))),ncol=2), 
+#             multipolar=matrix(c(na.omit(escoreU[escoreU[,3]>0.6 & freqU[,3]==1,1])
+#                                 ,rep("multipolar",length
+#                                      (na.omit(escoreU[escoreU[,3]>0.6 & freqU[,3]==1,1])))),ncol=2),
+#             pancake=matrix(c(na.omit(escoreU[escoreU[,4]>0.77 & freqU[,4]==1,1])
+#                              ,rep("pancake",length
+#                                   (na.omit(escoreU[escoreU[,4]>0.77 & freqU[,4]==1,1])))),ncol=2),
+#             spancake=matrix(c(na.omit(escoreU[escoreU[,5]>0.6 & freqU[,5]==1,1])
+#                               ,rep("spancake",length
+#                                    (na.omit(escoreU[escoreU[,5]>0.6 & freqU[,5]==1,1])))),ncol=2),
+#             sticks=matrix(c(na.omit(escoreU[escoreU[,6]>1 & freqU[,6]==1,1])
+#                             ,rep("sticks",length
+#                                  (na.omit(escoreU[escoreU[,6]>1 & freqU[,6]==1,1])))),ncol=2))
