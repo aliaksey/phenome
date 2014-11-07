e8284e462d827ba3b7dbd3c01524b1ad7fe052f4
aliaksey <aliaksey.vasilevich@gmail.com>
2014-11-07 11:52:04 +0100

    more names features added

--- ph_find most optimal clustering technique.R
+++ ph_find most optimal clustering technique.R
@@ -4,7 +4,19 @@ library("lattice")
 library("ggplot2")
 library("foreach")
 library("doParallel")
+library(caret)
 load("Cell all data & ground truth scaled.RData")
+load("model selection svm.RData")
+#load("model selection rf.RData")
+# selecting cell names find by model simulation
+#svm
+model.svm.1<-predictors(model.sel.res.feat.svm[[1]])
+model.svm.2<-predictors(model.sel.res.feat.svm[[2]])
+model.svm.3<-predictors(model.sel.res.feat.svm[[3]])
+#rf
+# model.rf.1<-predictors(model.sel.res.feat.rf[[1]])
+# model.rf.2<-predictors(model.sel.res.feat.rf[[2]])
+# model.rf.3<-predictors(model.sel.res.feat.rf[[3]])
 ##selecting cell shape variables creating 3 class.unes ofv variables: 10 simple; Zernike; and selected by bootstrap
 all.names.temp<-names(image.cell.scale)
 ##selecting only class.unycal cell shapes
@@ -12,22 +24,27 @@ simple.cellshape.name<-all.names.temp[grepl("Cells_AreaShape", all.names.temp)
                                       & !grepl("Zernike", all.names.temp)&
                                              !grepl("Center", all.names.temp)&
                                              !grepl("Neighbors", all.names.temp)]
-
 simple.2<-simple.cellshape.name[!grepl("Orientation",simple.cellshape.name)]
 simple.3<-simple.2[!grepl("Euler",simple.2)]
 simple.4<-simple.3[!grepl("Major",simple.3)]
 simple.5<-simple.4[!grepl("Compact",simple.4)]
 # selecting only Zernike cell shapes
 zernike.cellshape.name<-all.names.temp[grepl("Zernike", all.names.temp)]
-# selecting cell names find by model simulation
-model.sel.names<-c("Cells_AreaShape_Extent", "Cells_AreaShape_Eccentricity","Cells_AreaShape_Area", 
-                   "Cells_RadialDistribution_RadialCV_Actin4Corr_3of4", "Cells_AreaShape_MinorAxisLength")
-##find and remove highly correlated features
+##selecting non coreelated cell features
+
+# descrCorr <- cor(as.matrix(trainDescr))
+# highCorr <- findCorrelation(descrCorr, 0.70)
+# trainDescr <- trainDescr[, -highCorr]
+# testDescr <- testDescr[, -highCorr]
+
+##putting all names togrther
+selnames<-list(Simple=simple.cellshape.name,Simple2=simple.2,Simple3=simple.3, Simple4=simple.4,
+               Simple5=simple.5, Zernike=zernike.cellshape.name, 
+               SVM1=model.svm.1,SVM2=model.svm.2,SVM3=model.svm.3)
+
 ## puttinh all ground truth data together
 grnd.truth<-c(grnd.truth.feat.scale, grnd.truth.img.scale)
 names(grnd.truth)<-c("feature 1 %","feature 2 %","feature 3 %","image 1 %","image 2 %","image 3 %")
-##putting all names togrther
-selnames<-list(Simple=simple.cellshape.name,Simple2=simple.2,Simple3=simple.3, Simple4=simple.4,Simple5=simple.5, Zernike=zernike.cellshape.name, Model=model.sel.names)
 ##setting names for clustering methods
 hclust.meth<-c("ward.D", "ward.D2", "single", "complete", "average", "mcquitty" , "median",  "centroid")
 ##setting metods for disstances
@@ -38,6 +55,11 @@ dist.meth<-c( "euclidean", "maximum", "manhattan",  "binary",  "minkowski") #"ca
 #registerDoParallel(cl)
 ###########################loop#############################################
 #resultaccuracy<-foreach(ih=1:length(grnd.truth)) %dopar%{
+
+
+kmres<-kmeans(scale(datapca),5)
+heatmap(as.matrix(table(data_mor$Class,kmres$cluster)),main="Kmeans")
+
 clustering_result_accuracy<-c()
 for(ih in 1:length(grnd.truth)){
 data.cl<-grnd.truth[[ih]]
@@ -78,9 +100,9 @@ clustering_result_accuracy<-as.data.frame(rbind(clustering_result_accuracy, cbin
 clustering_result_accuracy$accur<-as.numeric(as.character(clustering_result_accuracy$accur))
 clustering_result_accuracy[order(clustering_result_accuracy$accur),]
 plot(clustering_result_accuracy[order(clustering_result_accuracy$accur),"accur"])
-##validatio
-data.dist<-dist(grnd.truth[[3]][,simple.3], method="maximum")
-hclustres<-hclust(data.dist, method = "ward.D")
-plot(hclustres)
-uns.clusters<-cutree(hclustres,length(unique(class.un)))
-table(uns.clusters, grnd.truth[[3]][,"Class"])               
+# ##validation
+# data.dist<-dist(grnd.truth[[3]][,simple.3], method="maximum")
+# hclustres<-hclust(data.dist, method = "ward.D")
+# plot(hclustres)
+# uns.clusters<-cutree(hclustres,length(unique(class.un)))
+# table(uns.clusters, grnd.truth[[3]][,"Class"])               
