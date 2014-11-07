2f4924011006d1421b04a3e866fccfd7309d789e
aliaksey <aliaksey.vasilevich@gmail.com>
2014-11-07 10:26:13 +0100

    added random forest made more optimal

--- ph_find_set of features to use for clustering.R
+++ ph_find_set of features to use for clustering.R
@@ -4,52 +4,23 @@ load("Cell all data & ground truth scaled.RData")
 library(foreach)
 library(doParallel)
 library(caret)
-#setup parallel backend to use 8 processors
-cl<-makeCluster(8)
+#setup parallel backend to use all processors
+cl<-makeCluster(detectCores())
 registerDoParallel(cl)
 
 ##performing on features
-model.sel.res.feat <- vector("list",length(grnd.truth.feat.scale))
-result1<-foreach(k=1:length(grnd.truth.feat.scale)) %dopar%{
-library(caret)
-## creating models by backward selection
-#load caret library
-#load data features
-data_features<-grnd.truth.feat.scale[[k]][,!(colnames(grnd.truth.feat.scale[[k]])%in%c("FeatureIdx","Class"))]
-#load data classes
-data_class<-grnd.truth.feat.scale[[k]][,"Class"]
-inTrain <- createDataPartition(data_class, p = 3/4, list = FALSE); 
-#Divide the dataset in train and test sets
-#Create the Training Dataset for Descriptors 
-trainDescr <- data_features[inTrain,]
-testDescr <- data_features[-inTrain,]
-trainClass <- data_class[inTrain]
-testClass <- data_class[-inTrain]
-is.numeric(trainDescr)
-# remove the redundant features
-descrCorr <- cor(as.matrix(trainDescr))
-highCorr <- findCorrelation(descrCorr, 0.70)
-trainDescr <- trainDescr[, -highCorr]
-testDescr <- testDescr[, -highCorr]
-#perform backwards selection.
-svmProfile <- rfe(x=trainDescr, y = trainClass, sizes = c(1:20), 
-rfeControl= rfeControl(functions = caretFuncs,number = 200),method = "svmRadial",fit = FALSE)
-model.sel.res.feat[[k]]<-svmProfile
-
-}
-stopCluster(cl)
-##performing on images
-#setup parallel backend to use 8 processors
-cl<-makeCluster(8)
-registerDoParallel(cl)
-
-model.sel.res.img <- vector("list",length(grnd.truth.img.scale))
-result2<-foreach(k=1:length(grnd.truth.img.scale)) %dopar%{
+model.sel.res.feat.svm<- vector("list",length(grnd.truth.feat.scale))
+model.sel.res.feat.svm<-foreach(k=1:length(grnd.truth.feat.scale)) %dopar%{
   library(caret)
+  
+  library("ipred")
+  library("gbm")
+  ## creating models by backward selection
+  #load caret library
   #load data features
-  data_features<-grnd.truth.img.scale[[k]][,!(colnames(grnd.truth.img.scale[[k]])%in%c("FeatureIdx","Class"))]
+  data_features<-grnd.truth.feat.scale[[k]][,!(colnames(grnd.truth.feat.scale[[k]])%in%c("FeatureIdx","Class"))]
   #load data classes
-  data_class<-grnd.truth.img.scale[[k]][,"Class"]
+  data_class<-grnd.truth.feat.scale[[k]][,"Class"]
   inTrain <- createDataPartition(data_class, p = 3/4, list = FALSE); 
   #Divide the dataset in train and test sets
   #Create the Training Dataset for Descriptors 
@@ -57,15 +28,85 @@ result2<-foreach(k=1:length(grnd.truth.img.scale)) %dopar%{
   testDescr <- data_features[-inTrain,]
   trainClass <- data_class[inTrain]
   testClass <- data_class[-inTrain]
-  # remove the redundant features
-  descrCorr <- cor(as.matrix(trainDescr))
-  highCorr <- findCorrelation(descrCorr, 0.70)
-  trainDescr <- trainDescr[, -highCorr]
-  testDescr <- testDescr[, -highCorr]
+  is.numeric(trainDescr)
+  # # remove the redundant features
+  # descrCorr <- cor(as.matrix(trainDescr))
+  # highCorr <- findCorrelation(descrCorr, 0.70)
+  # trainDescr <- trainDescr[, -highCorr]
+  # testDescr <- testDescr[, -highCorr]
   #perform backwards selection.
   svmProfile <- rfe(x=trainDescr, y = trainClass, sizes = c(1:20), 
                     rfeControl= rfeControl(functions = caretFuncs,number = 200),method = "svmRadial",fit = FALSE)
-  model.sel.res.img[[k]]<-svmProfile
+  model.sel.res.feat[[k]]<-svmProfile
 }
 stopCluster(cl)
-save(model.sel.res.img, model.sel.res.feat, file="model selection result.RData")
+save(model.sel.res.feat.svm, file="model selection svm.RData")
+
+cl<-makeCluster(detectCores())
+registerDoParallel(cl)
+model.sel.res.feat.rf<- vector("list",length(grnd.truth.feat.scale))
+model.sel.res.feat.rf<-foreach(k=1:length(grnd.truth.feat.scale)) %dopar%{
+  library(caret)
+  library("randomForest")
+  library("ipred")
+  library("gbm")
+  ## creating models by backward selection
+  #load caret library
+  #load data features
+  data_features<-grnd.truth.feat.scale[[k]][,!(colnames(grnd.truth.feat.scale[[k]])%in%c("FeatureIdx","Class"))]
+  #load data classes
+  data_class<-grnd.truth.feat.scale[[k]][,"Class"]
+  inTrain <- createDataPartition(data_class, p = 3/4, list = FALSE); 
+  #Divide the dataset in train and test sets
+  #Create the Training Dataset for Descriptors 
+  trainDescr <- data_features[inTrain,]
+  testDescr <- data_features[-inTrain,]
+  trainClass <- data_class[inTrain]
+  testClass <- data_class[-inTrain]
+  is.numeric(trainDescr)
+  # # remove the redundant features
+  # descrCorr <- cor(as.matrix(trainDescr))
+  # highCorr <- findCorrelation(descrCorr, 0.70)
+  # trainDescr <- trainDescr[, -highCorr]
+  # testDescr <- testDescr[, -highCorr]
+  #perform backwards selection.
+  rfProfile <- rfe(x=trainDescr, y = trainClass, sizes = c(1:20), 
+                   rfeControl= rfeControl(functions = rfFuncs))
+  model.sel.res.feat[[k]]<-rfProfile
+}
+stopCluster(cl)
+save(model.sel.res.feat.rf, file="model selection rf.RData")
+
+
+##performing on images
+#setup parallel backend to use 8 processors
+# cl<-makeCluster(8)
+# registerDoParallel(cl)
+# 
+# model.sel.res.img <- vector("list",length(grnd.truth.img.scale))
+# result2<-foreach(k=1:length(grnd.truth.img.scale)) %dopar%{
+#   library(caret)
+#   #load data features
+#   data_features<-grnd.truth.img.scale[[k]][,!(colnames(grnd.truth.img.scale[[k]])%in%c("FeatureIdx","Class"))]
+#   #load data classes
+#   data_class<-grnd.truth.img.scale[[k]][,"Class"]
+#   inTrain <- createDataPartition(data_class, p = 3/4, list = FALSE); 
+#   #Divide the dataset in train and test sets
+#   #Create the Training Dataset for Descriptors 
+#   trainDescr <- data_features[inTrain,]
+#   testDescr <- data_features[-inTrain,]
+#   trainClass <- data_class[inTrain]
+#   testClass <- data_class[-inTrain]
+#   # remove the redundant features
+#   descrCorr <- cor(as.matrix(trainDescr))
+#   highCorr <- findCorrelation(descrCorr, 0.70)
+#   trainDescr <- trainDescr[, -highCorr]
+#   testDescr <- testDescr[, -highCorr]
+#   #perform backwards selection.
+#   svmProfile <- rfe(x=trainDescr, y = trainClass, sizes = c(1:20), 
+#                     rfeControl= rfeControl(functions = caretFuncs,number = 200),method = "svmRadial",fit = FALSE)
+#   model.sel.res.img[[k]]<-svmProfile
+# }
+# stopCluster(cl)
+
+#save(model.sel.res.img, model.sel.res.feat, file="model selection result.RData")
