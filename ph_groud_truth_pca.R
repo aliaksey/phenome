aacbe8703844c1fc620a43bc5c898325194f0bd6
aliaksey <aliaksey.vasilevich@gmail.com>
2014-11-07 16:25:16 +0100

    script that calculate pca ground truth and all data in 3 different combinations

--- ph_groud_truth_pca.R
+++ ph_groud_truth_pca.R
@@ -39,48 +39,35 @@ feat.sc.dat.simp<-feature.cell.scale[,mngfll.names.feat.simple]
 ##join evrything to the list
 all.dat<-list(ImageAll=image.sc.dat.all,FeatureAll=feat.sc.dat.all,ImageZernike=image.sc.dat.zern,
               FeatureZernike=feat.sc.dat.zern,ImageSimple=image.sc.dat.simp,FeatureSimple=feat.sc.dat.simp)
+##calculate pca for allunits
+pca.results.all<-lapply(all.dat,function(x) prcomp(x,center=F, scale=F))
 ##putting evrything to a loop
-ground.truth.pca<-list()
+ground.truth.pca<-c()
 for(k in 1:length(all.dat)){
 #performing PCA 
   pca.res<-prcomp(all.dat[[k]],center=F, scale=F)
   if(grepl("Image",names(all.dat[k]))){
-    pca.res.m<-cbind(image.cell.scale[,"ImageNumber"],pca.res$x)
+    pca.res.m<-as.data.frame(cbind(ImageNumber=image.cell.scale[,"ImageNumber"],pca.res$x))
   }else{
-    pca.res.m<-cbind(feature.cell.scale[,"FeatureIdx"],pca.res$x)
+    pca.res.m<-as.data.frame(cbind(FeatureIdx=feature.cell.scale[,"FeatureIdx"],pca.res$x))
   }
   if(grepl("Image",names(all.dat[k]))){
     results.temp<-lapply(lapply(grnd.truth.img.scale,'[',c("Class","ImageNumber")),
   function(x)merge(x,pca.res.m, by="ImageNumber"))
 }else{
-  results.temp<-lapply(lapply(grnd.truth.img.scale, '[',c("Class","FeatureIdx")),
+  results.temp<-lapply(lapply(grnd.truth.feat.scale, '[',c("Class","FeatureIdx")),
              function(x)merge(x,pca.res.m, by="FeatureIdx"))
 }
+if(grepl("Image",names(all.dat[k]))){
+names(results.temp)<-apply(expand.grid(c("image 1%","image 2%","image 3%"), 
+                                       names(all.dat[k])), 1, paste, collapse=" ")
+}else{
+  names(results.temp)<-apply(expand.grid(c("feat 1%","feat 2%","feat 3%"), names(all.dat[k])), 1, paste, collapse=" ")
+  
+}
 ground.truth.pca<-c(ground.truth.pca,results.temp)
-}
 
-all.dat.pca<-lapply(all.dat, function(x) prcomp(x,center=F, scale=F))
-#merging pca scores with cell classes
-all.dat.pca.mer<-vector("list",6)
-for(k in c(1,3,5)) all.dat.pca.mer[[k]]<-cbind(all.dat.pca[[k]]$x,image.cell.scale[,"ImageNumber"])
-for(k in c(2,4,6)) all.dat.pca.mer[[k]]<-cbind(all.dat.pca[[k]]$x,feature.cell.scale[,"FeatureIdx"])
-
-all.dat.pca.mer.feature<-all.dat.pca.mer[c(2,4,6)]
-all.dat.pca.mer.image<-all.dat.pca.mer[c(1,3,5)]
-
-ground.truth.pca.feature<-lapply(all.dat.pca.mer.feature$x, 
-       function(x) merge(x,grnd.truth.feat.scale[[1]][,c("Class","FeatureIdx")] , by="FeatureIdx"))
-
-sapply(mylist, "[", c(2,3))
-grnd.truth.pca.scale<-list()
-for(k in 1:length(grnd.truth.feat.scale)){
-  m<-c(2,4,6)
-  yy<-
-  grnd.truth.pca.scale<-c(grnd.truth.pca.scale,yy)
 }
 
 
-all.dat.pca.mer<-all.dat.pca<-lapply(all.dat.pca, function(x) merge(x)
-
-plot(all.dat.pca[[6]])
-names(all.dat[2])
+save(ground.truth.pca, pca.results.all, file=" pca results for ground truth and all.RData")
