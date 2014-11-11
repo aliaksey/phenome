6947df11fa92b760237cfda46f163ac88346d084
aliaksey <aliaksey.vasilevich@gmail.com>
2014-11-11 15:37:26 +0100

    no change

--- ph_make_plots.R
+++ ph_make_plots.R
@@ -6,7 +6,5 @@ library(plyr)
 #################assesing accuracy of unsupervised methods
 load("accuracy_of_unsupervised_method.Rdata")
 plot(clust_accur_results$Accuracy)
-##find best approach
-ddply(clust_accur_results, .(DistanceMethod, ClusterMethod,
-                             FeatureNames ), summarise, Accmean=mean(Accuracy),2)
+
 
