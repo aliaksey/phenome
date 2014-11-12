6d0637583b192635b8f8bb8897bfa1a83a0f8835
aliaksey <aliaksey.vasilevich@gmail.com>
2014-11-12 16:50:00 +0100

    clustering all data

---
+++ ph_cluster_all_data.R
@@ -0,0 +1,58 @@
+rm(list=ls())
+load("Cell all data & ground truth scaled.RData")
+## selecting features
+all.names.temp<-names(image.cell.scale)
+
+all.names<-all.names.temp[!grepl("Object_Number", all.names.temp)&
+                            !grepl("FeatureIdx", all.names.temp)&
+                            !grepl("ImageNumber", all.names.temp)&
+                            !grepl("AreaShape_Center", all.names.temp)&
+                            !grepl("ImageQuality", all.names.temp)&
+                            !grepl("AreaShape_Center", all.names.temp)]
+##selecting only class.unycal cell shapes
+simple.cellshape.name<-all.names.temp[grepl("Cells_AreaShape", all.names.temp)
+                                      & !grepl("Zernike", all.names.temp)&
+                                        !grepl("Center", all.names.temp)&
+                                        !grepl("Neighbors", all.names.temp)]
+## create all possible combination of cell shape parameters to use in analysis 
+#combn(simple.cellshape.name,n)
+#do.call(paste, expand.grid(simple.cellshape.name,1:n))
+
+simple.2<-simple.cellshape.name[!grepl("Orientation",simple.cellshape.name)]
+simple.3<-simple.2[!grepl("Euler",simple.2)]
+simple.4<-simple.3[!grepl("Major",simple.3)]
+simple.5<-simple.4[!grepl("Compact",simple.4)]
+########setting parameters
+hclust.meth<-c("ward.D","ward.D2", "single", "complete", "average", "mcquitty" , "median",  "centroid")
+##setting metods for disstances hclust
+dist.meth<-c( "euclidean", "maximum", "manhattan",  "binary",  "minkowski") #"canberra",
+hclust.meth.u<-hclust.meth[1]
+dist.meth.u<-dist.meth[1]
+###################################performing clustering on control data set
+#calculating disctance matrix
+data.dist<-dist(grnd.truth.feat.scale[[3]][,simple.4], method=dist.meth.u)
+class.un<-grnd.truth.feat.scale[[3]][,"Class"]
+hclustres_con<-hclust(data.dist, method = hclust.meth.u)
+plot(hclustres_con)
+rect.hclust(hclustres_con,k=5)
+clusters_con<-cutree(hclustres_con,5)
+accur_mes_con<-table(clusters_con,class.un)
+max.col<-0
+accur_mes_con
+crop.matrix<-accur_mes_con
+for(k in 1:length(unique(class.un))){
+  max.column<-max(crop.matrix) 
+  max.col<-max.col+max.column
+  mxind<-as.data.frame((which(crop.matrix == max.column,arr.ind = T)))
+  if(length(mxind)>1) crop.matrix<-crop.matrix[-mxind[1,"clusters_con"],-mxind[1,"class.un"]] else break
+}
+accur<- as.numeric(max.col/sum(accur_mes_con))
+accur
+
+#performing clustering on all data set
+#calculating disctance matrix
+data.dist<-dist(feature.cell.scale[,simple.4], method=dist.meth.u)
+#performing clustering
+hclustres<-hclust(data.dist, method = hclust.meth.u)
+plot(hclustres)
+rect.hclust(hclustres,h=50)
