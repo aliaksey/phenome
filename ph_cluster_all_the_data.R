298402998cd6a7f80a9b56ccb5fe2d42897550e9
aliaksey <aliaksey.vasilevich@gmail.com>
2014-11-12 14:08:33 +0100

    added more

--- ph_cluster_all_the_data.R
+++ ph_cluster_all_the_data.R
@@ -23,25 +23,41 @@ simple.3<-simple.2[!grepl("Euler",simple.2)]
 simple.4<-simple.3[!grepl("Major",simple.3)]
 simple.5<-simple.4[!grepl("Compact",simple.4)]
 ########setting parameters
-hclust.meth<-c("ward.D", "ward.D2", "single", "complete", "average", "mcquitty" , "median",  "centroid")
+hclust.meth<-c("ward.D","ward.D2", "single", "complete", "average", "mcquitty" , "median",  "centroid")
 ##setting metods for disstances hclust
 dist.meth<-c( "euclidean", "maximum", "manhattan",  "binary",  "minkowski") #"canberra",
 hclust.meth.u<-hclust.meth[1]
 dist.meth.u<-dist.meth[1]
 #calculating disctance matrix
-data.dist<-dist(feature.cell.scale[,simple.5], method=dist.meth.u)
+data.dist<-dist(feature.cell.scale[,simple.4], method=dist.meth.u)
 #performing clustering
 hclustres<-hclust(data.dist, method = hclust.meth.u)
 plot(hclustres)
-rect.hclust(hclustres,h=50,)
+rect.hclust(hclustres,k=10,)
 #########do the same for control####################
 #calculating disctance matrix
-data.dist<-dist(grnd.truth.feat.scale[[3]][,simple.5], method=dist.meth.u)
+data.dist<-dist(grnd.truth.feat.scale[[3]][,simple.4], method=dist.meth.u)
+class.un<-grnd.truth.feat.scale[[3]][,"Class"]
 #performing clustering
 hclustres_con<-hclust(data.dist, method = hclust.meth.u)
 plot(hclustres_con)
+rect.hclust(hclustres_con,k=5,)
+clusters_con<-cutree(hclustres_con,5)
+accur_mes_con<-table(clusters_con,class.un)
+max.col<-0
+accur_mes_con
+crop.matrix<-accur_mes_con
+for(k in 1:length(unique(class.un))){
+  max.column<-max(crop.matrix) 
+  max.col<-max.col+max.column
+  mxind<-as.data.frame((which(crop.matrix == max.column,arr.ind = T)))
+  if(length(mxind)>1) crop.matrix<-crop.matrix[-mxind[1,"clusters_con"],-mxind[1,"class.un"]] else break
+}
+accur<- as.numeric(max.col/sum(accur_mes_con))
+accur
+
 ########saving all clusters together##############
 
-Classes<-cutree(scphcl.hclust,5)
+
 
 
