f7e3d61352e96772aabab9ec5d8414eb5cc55a5b
aliaksey <aliaksey.vasilevich@gmail.com>
2014-11-13 15:52:32 +0100

    find most different surfaces from dataset

--- ph_find_outliers_from_all_data_set.R
+++ ph_find_outliers_from_all_data_set.R
@@ -1,5 +1,25 @@
-k <- 1000 # Number of outliers from the population we want
-n <- length(x)
-ma.dist <- mahalanobis(x, colMeans(x), cov(x))
-ix <- order(ma.dist)
-mdf <- x[ix >= n - k]
+rm(list=ls())
+library(chemometrics)
+load("joined scaled data.RData")
+load("Cell all data & ground truth scaled.RData")
+#loading names
+all.names.temp<-names(image.cell.scale)
+all.names<-all.names.temp[!grepl("Object_Number", all.names.temp)&
+                            !grepl("FeatureIdx", all.names.temp)&
+                            !grepl("ImageNumber", all.names.temp)&
+                            !grepl("AreaShape_Center", all.names.temp)&
+                            !grepl("ImageQuality", all.names.temp)&
+                            !grepl("AreaShape_Center", all.names.temp)]
+##selecting only class.unycal cell shapes
+simple.name<-all.names.temp[grepl("Cells_AreaShape", all.names.temp)
+                                      & !grepl("Zernike", all.names.temp)&
+                                        !grepl("Center", all.names.temp)&
+                                        !grepl("Neighbors", all.names.temp)]
+
+dt.outlrs<-feature.cell[,simple.name]
+rownames(dt.outlrs)<-feature.cell[,"FeatureIdx"]
+mdres<-Moutlier(dt.outlrs, quantile = 0.99, plot = T)
+rsltmd<-dt.outlrs[mdres$rd < mdres$cutoff,]
+features.outliers<-names(rsltmd)
+outliers.to.plot<-cbind(features.outliers,seq(1:lenght(features.outliers)))
+colnames(outliers.to.plot)<-c("FeatureIdx", "Cluster")
