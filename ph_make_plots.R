972b0b2411486c7bca42239ba1dcf663671ec31f
aliaksey <aliaksey.vasilevich@gmail.com>
2014-12-11 16:46:34 +0100

    added density

--- ph_make_plots.R
+++ ph_make_plots.R
@@ -1,5 +1,6 @@
 rm(list=ls())
 library(plyr)
+library(ggplot2)
 ##plot cell number per repeat before and after filter
 #for unfiltered data
 par(mfrow=c(2,2))
@@ -29,16 +30,15 @@ hist(cell.dns.f$Image_Count_Cells, breaks = 100,xlab = "Number of cells per repe
 ##make plot area perimeter scattering after and before filter
 load("area_perimeter_plot.RData")
 par(mfrow=c(1,2))
-plot(cell.area$Cells_AreaShape_Perimeter~cell.area$Cells_AreaShape_Area, main="Before Filter",
-     xlab="Area",ylab="Perimeter")
-plot(cell.area.f$Cells_AreaShape_Perimeter~cell.area.f$Cells_AreaShape_Area, main="After Filter",
-     xlab="Area",ylab="Perimeter")
+ggplot(cell.area,aes(x=Cells_AreaShape_Area,y=Cells_AreaShape_Perimeter)) + geom_point() + geom_density2d()+
+  labs(x="Area",y="Perimeter",title ="Before Filter")
 
-
-
+ggplot(cell.area.f,aes(x=Cells_AreaShape_Area,y=Cells_AreaShape_Perimeter)) + geom_point() + geom_density2d()+
+  labs(x="Area",y="Perimeter",title ="After Filter")
 #################assesing accuracy of unsupervised methods
 par(mfrow=c(1,1))
 load("accuracy_of_unsupervised_method.Rdata")
-plot(clust_accur_results$Accuracy)
-
+plot(clust_accur_results$Accuracy,main="Accuracy of Unsupervised methods")
+## plot for mahalanobis
+load("mahalanobis_filtration_plot.RData")
 
