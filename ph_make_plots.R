5997a2533cdb0c65144fa500aff0221250e97ec4
aliaksey <aliaksey.vasilevich@gmail.com>
2014-12-11 17:06:29 +0100

    new layout

--- ph_make_plots.R
+++ ph_make_plots.R
@@ -1,6 +1,7 @@
 rm(list=ls())
 library(plyr)
 library(ggplot2)
+library(gridExtra)
 ##plot cell number per repeat before and after filter
 #for unfiltered data
 par(mfrow=c(2,2))
@@ -30,11 +31,12 @@ hist(cell.dns.f$Image_Count_Cells, breaks = 100,xlab = "Number of cells per repe
 ##make plot area perimeter scattering after and before filter
 load("area_perimeter_plot.RData")
 par(mfrow=c(1,2))
-ggplot(cell.area,aes(x=Cells_AreaShape_Area,y=Cells_AreaShape_Perimeter)) + geom_point() + geom_density2d()+
+g1<-ggplot(cell.area,aes(x=Cells_AreaShape_Area,y=Cells_AreaShape_Perimeter)) + geom_point() + geom_density2d()+
   labs(x="Area",y="Perimeter",title ="Before Filter")
 
-ggplot(cell.area.f,aes(x=Cells_AreaShape_Area,y=Cells_AreaShape_Perimeter)) + geom_point() + geom_density2d()+
+g2<-ggplot(cell.area.f,aes(x=Cells_AreaShape_Area,y=Cells_AreaShape_Perimeter)) + geom_point() + geom_density2d()+
   labs(x="Area",y="Perimeter",title ="After Filter")
+grid.arrange(g1,g2,nrow=1, ncol=2)
 #################assesing accuracy of unsupervised methods
 par(mfrow=c(1,1))
 load("accuracy_of_unsupervised_method.Rdata")
@@ -42,3 +44,9 @@ plot(clust_accur_results$Accuracy,main="Accuracy of Unsupervised methods")
 ## plot for mahalanobis
 load("mahalanobis_filtration_plot.RData")
 
+g3<-ggplot(cell.shape,aes(y=Cells_AreaShape_Compactness,x=Cells_AreaShape_Solidity)) + geom_point() + geom_density2d()+
+  labs(x="Solidity",y="Compactness",title ="Before Filter")
+
+g4<-ggplot(cell.shape.f,aes(y=Cells_AreaShape_Compactness,x=Cells_AreaShape_Solidity)) + geom_point() + geom_density2d()+
+  labs(x="Solidity",y="Compactness",title ="After Filter")
+grid.arrange(g3,g4,nrow=1, ncol=2)
