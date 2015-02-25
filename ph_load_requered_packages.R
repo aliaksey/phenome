581c3e7f61f3fb5e1bbaaf34b2214ba1503287ea
aliaksey <aliaksey.vasilevich@gmail.com>
2015-02-25 09:53:24 +0100

    added package

--- ph_load_requered_packages.R
+++ ph_load_requered_packages.R
@@ -1,5 +1,5 @@
 
-list.of.packages <- c("caret","chemometrics", "corrplot","data.table" ,"doParallel" ,"dplyr" ,"foreach",
+list.of.packages <- c("caret","dendextend","chemometrics", "corrplot","data.table" ,"doParallel" ,"dplyr" ,"foreach",
                       "ggplot2" ,  "gplots" ,"gridExtra" ,"Rcpp","mclust","microbenchmark"   ,"party"  ,"pROC" ,"Rcpp", 
                       "Rcpp",   "reshape" ,"reshape2","rgl","cluster","robustbase", "scales" , "grid"  ,"rpart" , "lattice")
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
